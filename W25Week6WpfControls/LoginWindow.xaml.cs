4f0b9c0230d35d8bcb18d7251429428072d450cc
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-10 14:30:11 -0500

    login ex

---
+++ W25Week6WpfControls/LoginWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace W25Week6WpfControls
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private string username = "abc";
+        private string password = "abc";
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == username && pswPassword.Password == password)
+            {
+                MainWindow win = new MainWindow();
+                win.Show();
+                Close();
+            }
+            else
+                MessageBox.Show("Invalid login. Please try again.");
+        }
+    }
+}
